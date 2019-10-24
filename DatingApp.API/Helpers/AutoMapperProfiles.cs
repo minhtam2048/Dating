228efbe8a22274cee3afbb46f456145141286f7e
minhtam2048 <minhtam2048@gmail.com>
2019-10-24 17:17:31 +0700

    finished basic user authentication

---
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,12 @@
+using AutoMapper;
+
+namespace DatingApp.API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            
+        }
+    }
+}
