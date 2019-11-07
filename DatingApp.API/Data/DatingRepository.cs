63d13f41642704496efe2501baed3643c3140ce7
minhtam2048 <minhtam2048@gmail.com>
2019-11-07 19:49:08 +0700

    Added Like function

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -37,6 +37,18 @@ namespace DatingApp.API.Data
 
             users = users.Where(u => u.Id != userParams.UserId);
             users = users.Where(u => u.Gender == userParams.Gender);
+            
+            if(userParams.Likers)
+            {
+                var userLikers = await GetUserLikes(userParams.UserId, userParams.Likers);
+                users = users.Where(u => userLikers.Contains(u.Id));
+            }
+
+            if(userParams.Likees)
+            {
+                var userLikees = await GetUserLikes(userParams.UserId, userParams.Likers);
+                users = users.Where(u => userLikees.Contains(u.Id));
+            }
 
             if(userParams.MinAge != 18 || userParams.MaxAge != 99) 
             {
@@ -73,9 +85,29 @@ namespace DatingApp.API.Data
             return photo;   
         }
 
+        public async Task<Like> GetLike(int userId, int recipientId)
+        {
+            return await _context.Likes.FirstOrDefaultAsync(u => u.LikerId == userId && u.LikeeId == recipientId);
+        }
+
+        private async Task<IEnumerable<int>> GetUserLikes(int id, bool likers)
+        {
+            var user = await _context.Users.Include(x => x.Likers).Include(x => x.Likees).FirstOrDefaultAsync(u => u.Id == id);
+            
+            if (likers)
+            {
+                return user.Likers.Where(u => u.LikeeId == id).Select(i => i.LikerId);
+            }
+            else 
+            {
+                return user.Likees.Where(u => u.LikerId == id).Select(i => i.LikeeId);
+            }
+
+        }
+
         public async Task<bool> SaveAll()
         {
-            return await _context.SaveChangesAsync() > 0; //whatever change happened, save it   
+            return await _context.SaveChangesAsync() > 0; // whatever change happened, save it   
         }
     }
 }
